dbc8c8d70ccc497351b7de19dbee0cdab34750c4
Vincent Battaglia <vincent.battaglia@gmail.com>
2026-02-16 21:28:18 +0100

    Vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: undefined,
-      precompress: false,
-      strict: true
-    })
-  }
+    adapter: adapter(),
+  },
 };
 
 export default config;
